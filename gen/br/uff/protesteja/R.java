74837d60d5f4f963f59af178c561956877570921
Wellington <ton@ton-Aspire-V3-571.(none)>
2013-11-27 00:41:21 -0200

    criando activity update - ainda não codificada

--- gen/br/uff/protesteja/R.java
+++ gen/br/uff/protesteja/R.java
@@ -37,25 +37,32 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int activity_send_post=0x7f030001;
         public static final int activity_show_posts=0x7f030002;
+        public static final int activity_update=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
         public static final int send_post=0x7f070001;
         public static final int show_posts=0x7f070002;
+        public static final int update=0x7f070003;
+        public static final int update_delete=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int btn_atualizar=0x7f050006;
-        public static final int btn_enviar=0x7f050007;
-        public static final int btn_postar=0x7f050004;
-        public static final int btn_ver=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int lbl_nome=0x7f05000a;
-        public static final int lbl_protesto=0x7f05000b;
-        public static final int title=0x7f050003;
-        public static final int title_activity_send_post=0x7f050009;
-        public static final int title_activity_show_posts=0x7f050008;
+        public static final int btn_atualizar=0x7f050005;
+        public static final int btn_delete=0x7f05000d;
+        public static final int btn_enviar=0x7f050006;
+        public static final int btn_postar=0x7f050003;
+        public static final int btn_update=0x7f05000c;
+        public static final int btn_ver=0x7f050004;
+        public static final int hello_world=0x7f05000f;
+        public static final int lbl_nome=0x7f050009;
+        public static final int lbl_protesto=0x7f05000a;
+        public static final int title=0x7f050002;
+        public static final int title_activity_send_post=0x7f050008;
+        public static final int title_activity_show_posts=0x7f050007;
+        public static final int title_activity_update=0x7f05000e;
+        public static final int title_activity_update_delete=0x7f05000b;
     }
     public static final class style {
         /** 
